a9bf9dca647935d0b891be76303dff9d5d31752f
Thiago Mandrik <thiagomandrik@gmail.com>
2023-07-17 20:36:43 -0300

    remoção do static dir

--- setup/settings.py
+++ setup/settings.py
@@ -116,12 +116,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'setup/static')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
